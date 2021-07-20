1abd6ceae3cf57f4941223c615e3285cbbd95ae9
Samk13 <samarbid13@gmail.com>
2021-07-20 23:27:05 +0200

    implement get and post api routes on prfile page

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,37 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+type Data = {
+  name?: string
+  errorMessage?: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method === 'POST') {
+    const reqData = req.body.name.split(' ')
+    if (!arrIsNumeric(reqData))
+      res.status(400).json({ errorMessage: 'Please enter a number' })
+    else
+      res.status(200).json({
+        name: req.body.name.trim().split(' '),
+      })
+  } else {
+    res.status(200).json({ name: 'John Doe' })
+  }
+}
+
+// check if the request is numeric
+const isNumber = (req: string) => {
+  return Number.isInteger(Number(req))
+}
+// check if input is string
+const isString = (req: string) => {
+  return typeof req === 'string'
+}
+
+const arrIsNumeric = (arr: string[]) => {
+  return arr.every(isNumber)
 }
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,8 +1,66 @@
-const Profile: React.FC = () => {
+import { GetStaticProps } from 'next'
+import React from 'react'
+
+interface ProfileProps {
+    response: any
+}
+
+const Profile: React.FC<ProfileProps> = ({response}) => {
+    const [res, setRes] = React.useState([])
+    const [errors, setErrors] = React.useState([])
+        const registerUser = async event => {
+          event.preventDefault()
+          console.log(event.target.name.value);
+          const res = await fetch(
+            'http://localhost:3000/api/hello',
+            {
+              body: JSON.stringify({
+                name: event.target.name.value
+              }),
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              method: 'POST'
+            }
+          )
+
+          const result = await res.json()
+          console.log(result);
+          setErrors([])
+        if (result.errorMessage) {
+            setErrors(result.errorMessage)
+        } else setRes([...result.name])
+        }
+        console.log(res);
+
+
     return (
         <div>
-            <h1>Profile</h1>
+            <h1>Merge and sort</h1>
+            <p>call the next API</p>
+            <div>{response.name}</div>
+            <form onSubmit={registerUser}>
+                <label htmlFor="name">Name</label>
+                <input id="name" type="text" autoComplete="name" required />
+                <button type="submit">Register</button>
+            </form>
+            <p>result:</p>
+            <ul>
+                errors: {errors ? errors: ""}
+            {res.map((item, index) => (<li key={index}>{item}<hr/></li>))}
+            </ul>
         </div>
     )
 }
+const getStaticProps: GetStaticProps = async () => {
+    const posts = await fetch('http://localhost:3000/api/hello')
+    const data = await posts.json()
+    return {
+      props: {
+        response: data
+      },
+    }
+  }
+
 export default Profile
+export {getStaticProps}
